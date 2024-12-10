7379bda64502553206574c56d19d04b865468191
ricky-sensei <axcelcamp.teacher@gmail.com>
2024-12-10 20:08:10 +0900

    おみくじアプリ完成

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import random
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/omikuji')
 def index():
-    result = "大吉"
+    result = random.choice(["大吉!!", "吉", "凶..."])
     return render_template("omikuji.html", result=result)
 
 if __name__ == '__main__':
